93e5666e52e67a79e8bcaf7ce780457ee25f5c7b
MarlyMazing <45586481+MarlyMazing@users.noreply.github.com>
2018-12-04 20:48:15 -0500

    Added the TravelAccount class

--- src/main/java/DataBase.java
+++ src/main/java/DataBase.java
@@ -4,5 +4,23 @@ import java.util.Map;
 
 public class DataBase {
     private Map<String, File> bookingMap = new HashMap<>();
-    private Map<String, TravelerAccount>
+    private Map<String, TravelerAccount> userMap = new HashMap<>();
+    private File bookingFile;
+    TravelerAccount travelerAccount;
+
+    public boolean addNewTravelerAccount(Traveler Account) {
+
+        return false;
+    }
+
+    public boolean removeTravelerAccount(String email) {
+        return false;
+    }
+
+    public TravelerAccount loginToTravelerAccount(String login){
+
+        return userMap.get(login);
+    }
+
+
 }
