64518046c95b4df0b1e06ba22d1d81b38ba5b845
MarlyMazing <45586481+MarlyMazing@users.noreply.github.com>
2018-12-04 20:53:57 -0500

    Added the TravelAccount class

---
+++ src/main/java/TravelerAccount.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.util.Date;
+import java.util.Map;
+
+public class TravelerAccount {
+    private String fullName;
+    private int address;
+    private String email;
+    private Map<Date, File> previousBookingsMap;
+
+}
