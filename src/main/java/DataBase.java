5e3d782c1abeb7a2545f42383e1de63a06f22b97
Marlon <45586481+MarlyMazing@users.noreply.github.com>
2018-12-06 00:55:47 -0500

    Updated DataBase and Traveler classes

--- src/main/java/DataBase.java
+++ src/main/java/DataBase.java
@@ -1,25 +1,53 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class DataBase {
     private Map<String, File> bookingMap = new HashMap<>();
     private Map<String, TravelerAccount> userMap = new HashMap<>();
-    private File bookingFile;
-    TravelerAccount travelerAccount;
 
-    public boolean addNewTravelerAccount(Traveler Account) {
-
-        return false;
+    public void addNewTravelerAccount(TravelerAccount travelerAccount) {
+        userMap.put(travelerAccount.getEmail(), travelerAccount);
     }
 
-    public boolean removeTravelerAccount(String email) {
-        return false;
+    public void removeTravelerAccount(String email) {
+        userMap.remove(email);
     }
 
-    public TravelerAccount loginToTravelerAccount(String login){
+    public TravelerAccount loginToTravelerAccount(String login) throws Exception, RuntimeException {
+           String inputEmail;
+           String inputPassword;
+           boolean successfulLogin = false;
+           TravelerAccount travelerAccount;
 
-        return userMap.get(login);
+           inputEmail = login.substring(0, login.indexOf(" ")-1); // Looks at what's before the space
+           inputPassword = login.substring(login.indexOf(" ")+1);// Looks at what's after the space
+
+           travelerAccount = userMap.get(inputEmail);
+
+               if (travelerAccount == null) {
+                   throw new Exception(); //Would replace with custom exception for incorrect email
+               }
+               else if (travelerAccount.getLoginCredentials()== login){
+                  successfulLogin = true;
+               }
+               else if (!successfulLogin) {
+                   throw new RuntimeException(); //Would replace with custom exception for incorrect email and password combination
+               }
+
+             /*Thinking about creating an exception for (email doesn't exist, incorrect password,
+                                        and a big maybe on too many login attempts) we may have to rethink how we use the email
+                                        and password in the DataBase however*/
+             return userMap.get(login);
+    }
+
+    public void addNewBookingData(String bookingNumber, File bookingFile) {
+    /*Have to rethink how I am going to implement. May need to add an additional parameter for JSON resource that we get
+    through api. May consider have private method that creates a folder and saves JSON file
+
+     */
+        bookingMap.put(bookingNumber, bookingFile);
     }
 
 
