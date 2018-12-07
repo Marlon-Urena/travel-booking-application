dadd669e9ed5e7e6ea5280ee2ea62bcff9e0f697
Briana-Connolly <32718020+Briana-Connolly@users.noreply.github.com>
2018-12-06 23:23:46 -0500

    Added to main to get a better sense of the flow of our program. From my understanding I think travelProgram will act as our "guide" for the entire program meaning it will control most of what the users do

--- src/main/java/TravelProgram.java
+++ src/main/java/TravelProgram.java
@@ -1,12 +1,30 @@
 public class TravelProgram {
 
 
-    public static void main(String [] args) {
-        Traveler traveler;
-        BookingDataBase database;
-        Schedule schedule;
-        Estimate estimate;
-        Booking booking;
+    public static void main(String [] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+
+        TravelProgram travelProgram = new TravelProgram();
+        DataBase database = new DataBase();
+
+        System.out.println("Welcome! Would you like to \n 1. Make a new account \n 2. Login");
+        int response = scan.nextInt();
+
+        System.out.println("Enter your email: ");
+        String email = scan.next();
+        System.out.println("Enter your password: ");
+        String password = scan.next();
+
+        TravelerAccount travelAccount = new TravelerAccount(email, password);
+        Traveler traveler = new Traveler();
+
+        if(response == 1){
+            travelProgram.createNewTravelerAccount(database, email);
+        }
+        else if(response == 2){
+            travelAccount = travelAccount.login(database, email);
+        }
     }
 
     public void makeNewBooking(Booking booking) {
