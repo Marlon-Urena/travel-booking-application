c7f3e667301f73cbe181ee7e9383b595abbcd9d1
Briana-Connolly <32718020+Briana-Connolly@users.noreply.github.com>
2018-12-11 18:27:18 -0500

    Added setAccountInfo Function and implemented retrieveBooking function

--- src/main/java/Traveler.java
+++ src/main/java/Traveler.java
@@ -1,3 +1,4 @@
+import javax.xml.crypto.Data;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -8,6 +9,7 @@ import java.util.Scanner;
         private TravelerAccount travelerAccount;
         private Schedule schedule;
         private String Login;
+        private Database database;
 
         public void makeRequest(Booking booking) {
             Scanner scan = new Scanner(System.in);
@@ -22,9 +24,47 @@ import java.util.Scanner;
             schedule.setLengthOfTrip(lengthOfTrip);
         }
 
-        public void changeAccountInfo() throws ParseException {
+        public void setAccountInfo(TravelerAccount travelerAccount) throws ParseException {
             Scanner scan = new Scanner(System.in);
-            System.out.println("Would you like to change your 1) email 2) password 3)date of birth ");
+            System.out.println("*********************");
+            System.out.println("Enter Account Info");
+            System.out.println("*********************");
+
+            System.out.print("\tEnter your first name: ");
+            String firstName = scan.nextLine();
+            travelerAccount.setFirstName(firstName);
+
+            System.out.print("\tEnter your last name: ");
+            String lastName = scan.nextLine();
+            travelerAccount.setLastName(lastName);
+
+            System.out.print("\tEnter your address: ");
+            String address = scan.nextLine();
+            travelerAccount.setAddressLine1(address);
+
+            System.out.print("\tEnter your city: ");
+            String city = scan.nextLine();
+            travelerAccount.setCity(city);
+
+            System.out.print("\tEnter your state: ");
+            String state = scan.nextLine();
+            travelerAccount.setState(state);
+
+            System.out.print("\tEnter your ZIP Code: ");
+            String zip = scan.nextLine();
+            travelerAccount.setZipCode(zip);
+
+            /*System.out.println("\tEnter your date of Birth: ");
+            String dateOfBirth = scan.next();
+            DateFormat formatter = new SimpleDateFormat("EEEE dd MMM yyyy");
+            Date date = formatter.parse(dateOfBirth);
+            travelerAccount.setDateOfBirth(date);*/
+
+        }
+
+        public void changeAccountInfo(TravelerAccount travelerAccount) throws ParseException {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Would you like to change your 1) email 2) password 3)date of birth 4) Go back");
             int response = scan.nextInt();
 
             if(response == 1){
@@ -42,15 +82,12 @@ import java.util.Scanner;
                 String newDate = scan.next();
                 DateFormat formatter = new SimpleDateFormat("EEEE dd MMM yyyy");
                 Date date = formatter.parse(newDate);
-                travelerAccount.changedateOfBirth(date);
-            }
-            else{
-                //quit
+                travelerAccount.changeDateOfBirth(date);
             }
         }
 
-        public void retrieveBookingInfo() {
-            //input booking number to database
+        public void retrieveBookingInfo(Database database, String bookingNumber) {
+            database.searchOldBookingData(bookingNumber);
         }
 
         public boolean login(TravelerAccount travelerAccount) {
