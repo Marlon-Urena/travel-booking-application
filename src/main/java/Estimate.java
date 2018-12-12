536a6c616c7bb75615136813f6c2a95687cbd4dd
Briana-Connolly <32718020+Briana-Connolly@users.noreply.github.com>
2018-12-12 00:07:09 -0500

    Updated Estimate to work with Budget class

--- src/main/java/Estimate.java
+++ src/main/java/Estimate.java
@@ -1,20 +1,21 @@
 public class Estimate {
-    private double budget;
+    Budget budget;
     private double totalCost = 0;
 
     //HashMap of how much each "activity" cost?
+    //Budget will now check if the traveler is going over budget. Estimate will provide the estimate.
 
-    public void setEstimate(double budget) {
+    public Estimate(Budget budget){
         this.budget = budget;
     }
 
     public void addToTotalCost(double amount){
-        totalCost += amount;
+        if(budget.isEstimateOverBudget(amount)) {
+            totalCost += amount;
+        }
     }
 
-
     public Double provideEstimate(){
-
         return totalCost;
     }
 }
