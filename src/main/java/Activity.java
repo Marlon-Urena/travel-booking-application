5ec8d1b4b1bf9da4abbfc7d087f9b8be955bb757
Briana-Connolly <32718020+Briana-Connolly@users.noreply.github.com>
2018-12-13 22:01:14 -0500

    Put activities in a constructor because they were originally put in methods that wouldn't be called

--- src/main/java/Activity.java
+++ src/main/java/Activity.java
@@ -6,24 +6,21 @@ public class Activity {
     private Map<Integer, String> activityMap = new HashMap<>();
 
 
-    public void addToActivityPriceMap(){
-       activityPriceMap.put("Skydiving", Double.valueOf(300));
-       activityPriceMap.put("Museum Visit", Double.valueOf(20));
-       activityPriceMap.put("Horseback Riding", Double.valueOf(60));
-       activityPriceMap.put("Surfing Lessons", Double.valueOf(50));
-       activityPriceMap.put("Spa", Double.valueOf(100));
-       activityPriceMap.put("Amusement Park", Double.valueOf(75));
-    }
+ public Activity(){
+    activityPriceMap.put("Skydiving", Double.valueOf(300));
+    activityPriceMap.put("Museum Visit", Double.valueOf(20));
+    activityPriceMap.put("Horseback Riding", Double.valueOf(60));
+    activityPriceMap.put("Surfing Lessons", Double.valueOf(50));
+    activityPriceMap.put("Spa", Double.valueOf(100));
+    activityPriceMap.put("Amusement Park", Double.valueOf(75));
 
-    public void addToActivityMap(){
-        activityMap.put(1, "Skydiving");
-        activityMap.put(2, "Museum Visit");
-        activityMap.put(3, "Horseback Riding");
-        activityMap.put(4, "Surfing Lessons");
-        activityMap.put(5, "Spa");
-        activityMap.put(6, "Amusement Park");
-    }
-
+    activityMap.put(1, "Skydiving");
+    activityMap.put(2, "Museum Visit");
+    activityMap.put(3, "Horseback Riding");
+    activityMap.put(4, "Surfing Lessons");
+    activityMap.put(5, "Spa");
+    activityMap.put(6, "Amusement Park");
+}
     public String getActivity(int activityNumber){
         return activityMap.get(activityNumber);
     }
