cae9649745715f40b27fd8ad7e053a83e9069ec8
Marlon <45586481+MarlyMazing@users.noreply.github.com>
2018-12-16 08:30:49 -0500

    Booking class will not receive a list of objects instead of strings

--- src/main/java/Booking.java
+++ src/main/java/Booking.java
@@ -3,7 +3,7 @@ import java.util.List;
 public abstract class Booking {
     protected String apiKey = "JDyCGQgGGU0GMzHNY8ck0b7vtZLwgpuz";
     public abstract void receiveRequest(Estimate estimate, Schedule schedule);
-    public abstract List<String> provideOptions(List<String> list); //Think about return type for this
+    public abstract List<Object> provideOptions(List<String> list); //Think about return type for this
 
 }
 
