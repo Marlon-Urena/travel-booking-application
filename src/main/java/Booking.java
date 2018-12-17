1c6c290d81700b1c7f7ef4758aaae7fc55075976
Marlon <45586481+MarlyMazing@users.noreply.github.com>
2018-12-17 14:48:40 -0500

    Edited to support exceptions thrown from TravelerProgram, same as other booking classes

--- src/main/java/Booking.java
+++ src/main/java/Booking.java
@@ -1,9 +1,11 @@
+import io.swagger.client.ApiException;
+
 import java.util.List;
 
 public abstract class Booking {
     protected String apiKey = "JDyCGQgGGU0GMzHNY8ck0b7vtZLwgpuz";
     public abstract void receiveRequest(Estimate estimate, Schedule schedule);
-    public abstract List<Object> provideOptions(List<String> list); //Think about return type for this
+    public abstract List<Object> provideOptions(List<String> list) throws ApiException; //Think about return type for this
 
 }
 
