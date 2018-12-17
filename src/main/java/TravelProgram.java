7299f158e0d672a68c4b393a7163a1842c1d8822
Marlon <45586481+MarlyMazing@users.noreply.github.com>
2018-12-17 14:57:32 -0500

    Tweaked a bunch of things

--- src/main/java/TravelProgram.java
+++ src/main/java/TravelProgram.java
@@ -1,9 +1,11 @@
+import io.swagger.client.ApiException;
 import io.swagger.client.api.DefaultApi;
 import org.apache.commons.validator.routines.EmailValidator;
 
 import java.awt.print.Book;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -40,7 +42,7 @@ public class TravelProgram{
         Scanner scanner = new Scanner(System.in);
 
             int selection;
-            System.out.println("Hello! \nWould you like to \n 1. Login \n 2. Make a new account ");
+            System.out.print("Hello! \nWould you like to \n 1. Login \n 2. Make a new account \nSelection: ");
             selection = scanner.nextInt();
             scanner.nextLine();
             do {
@@ -63,16 +65,16 @@ public class TravelProgram{
                     case CREATE_BOOKING:{
                         System.out.print("Set budget for your trip: ");
                         double tripBudget = scanner.nextDouble();
-                        traveler.setBudget(tripBudget);
+                        traveler.setBudget(tripBudget); //I can use the budget for trips
 
-                        displayBookingMenu(scanner, database, traveler, bookingOptions, estimate, activity); //Only passing flightBooking but what about carRentalBooking and the other bookings?
+                        displayBookingMenu(scanner, database, traveler, bookingOptions, estimate, activity);
                     }
 
                         break;
                     case SEARCH_BOOKING:{
-                        System.out.println("Enter booking number: ");
+                        System.out.print("Enter booking number: ");
                         String bookingNum = scanner.nextLine();
-                        traveler.retrieveBookingInfo(database, bookingNum);
+                        System.out.println(traveler.retrieveBookingInfo(database, bookingNum));
                     }
                         break;
                     case CHANGE_INFO:
@@ -111,7 +113,7 @@ public class TravelProgram{
 
         return selection;
     }
-    public static void makeNewBooking(Traveler traveler, Object booking) {
+    public static void makeNewBooking(Traveler traveler, Object booking) { //Remove
 
     }
 
@@ -132,6 +134,7 @@ public class TravelProgram{
             System.out.print("\nEnter your selection:");
             
             int response = scanner.nextInt();
+            scanner.nextLine();
             
             if(response == 1){displayFlightBookingMenu(scanner, database, traveler, bookingOptions.get(response-1));}
             else if(response == 2){displayCarRentalMenu(scanner, database,  traveler, bookingOptions.get(response-1));}
@@ -155,7 +158,7 @@ public class TravelProgram{
         List <Object> flightOptions;
         int selection;
 
-        System.out.println("Enter search parameters below. Leave unwanted parameters blank.");
+        System.out.println("Enter search parameters below. Leave unwanted parameters blank.\n");
         System.out.print("(Required) Travel from: ");
         searchParams.add(scanner.nextLine());
         System.out.print("(Required) Travel to: ");
@@ -186,15 +189,29 @@ public class TravelProgram{
         System.out.print("(Optional) Number of results to display: ");
         searchParams.add(scanner.nextLine());
 
-        flightOptions = flightBooking.provideOptions(searchParams);
-        for (int i = 0; i < flightOptions.size(); i++) {
-            System.out.println("Itinerary #" + i+1);
-            System.out.println(flightOptions.get(i));
+        try {
+            flightOptions = flightBooking.provideOptions(searchParams); // Change implementation to check to see if an error has occurred and give user option to not decide on anything. We can do if statement to test for 0 to exit and out of bounds number to retry input
+            for (int i = 0; i < flightOptions.size(); i++) {
+                System.out.println("\n\nItinerary #" + i + 1);
+                System.out.println(flightOptions.get(i));
+            }
+            System.out.print("Enter the number next to your desired itinerary: ");
+            selection = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Your Booking Number is: " + traveler.makeRequest(flightOptions.get(selection - 1), database));
+
         }
-        System.out.println("Enter the number next to your desired itinerary: ");
-        selection = scanner.nextInt();
-        traveler.makeRequest(flightOptions.get(selection), database);
-        scanner.nextLine();
+        catch (ApiException e) {
+            String responseString = e.getResponseBody();
+            if (responseString != null) {
+                int beginLoc = responseString.indexOf("message") + 12;
+                int endLoc = responseString.indexOf("\"\n}");
+                String exceptionErrorMessage = responseString.substring(beginLoc, endLoc);
+                System.out.println(exceptionErrorMessage);
+            }
+            System.out.println("Unable to connect to flight system. Please try later.");
+
+        }
 
         //traveler.makeRequest(flightOptions.get(selection-1)); //Will request the selected booking but will check to see if it fits within Traveler's budget
 
@@ -218,16 +235,28 @@ public class TravelProgram{
         searchParams.add(scanner.nextLine());
         //System.out.print("(Optional) Enter your 2 character country code (e.g. US,FR,IT): ");
         //searchParams.add(scanner.nextLine());
-        carRentalOptions = carRentalBooking.provideOptions(searchParams);
+        try {
+            carRentalOptions = carRentalBooking.provideOptions(searchParams);
 
-        for(int i = 0; i < carRentalOptions.size(); i++) {
-            System.out.println("Option #" + i+1);
-            System.out.println(carRentalOptions.get(i));
+            for (int i = 0; i < carRentalOptions.size(); i++) {
+                System.out.println("Option #" + i + 1);
+                System.out.println(carRentalOptions.get(i));
+            }
+            System.out.println("Enter the number next to your desired option: ");
+            selection = scanner.nextInt();
+            traveler.makeRequest(carRentalOptions.get(selection), database);
+            scanner.nextLine();
+        }catch (ApiException e) {
+            String responseString = e.getResponseBody();
+            if (responseString != null) {
+                int beginLoc = responseString.indexOf("message") + 12;
+                int endLoc = responseString.indexOf("\"\n}");
+                String exceptionErrorMessage = responseString.substring(beginLoc, endLoc);
+                System.out.println(exceptionErrorMessage);
+
+            }
+            System.out.println("Unable to connect to flight system. Please try later.");
         }
-        System.out.println("Enter the number next to your desired option: ");
-        selection = scanner.nextInt();
-        traveler.makeRequest(carRentalOptions.get(selection), database);
-        scanner.nextLine();
 
 
     }
@@ -244,6 +273,8 @@ public class TravelProgram{
     public static TravelerAccount loginToTravelerAccount(TravelerAccount travelerAccount, Database database, Scanner scanner) {
         String email;
         String password;
+        int numberOfSignInAttempts = 0;
+        int selection;
 
         boolean validEntry =false;
 
@@ -263,11 +294,26 @@ public class TravelProgram{
                 travelerAccount = travelerAccount.login(database,email, password);
                 validEntry = true;
             } catch (RuntimeException e) {
-                System.out.println("\t"+e);
+                System.out.println("\t INCORRECT EMAIL AND PASSWORD COMBINATION.\n");
             } catch (Exception e) {
-                System.out.println("\t"+ e);
+                System.out.println("\t EMAIL DOESN'T EXIST. PLEASE TRY AGAIN.\n");
             }
+            /*
+            numberOfSignInAttempts++; //Think about a way to get user out of this look if he doesn't remember his sign in info
+            if (numberOfSignInAttempts >= 3) {
+                    System.out.println("Create Account? 1=yes, 2=no \n Selection: ");
+                    try {
+                        selection = scanner.nextInt();
+                        if (selection == 1) {
+                            createNewTravelerAccount(travelerAccount, database, scanner);
+                        }
+                    } catch (InputMismatchException e) {
+                        System.out.println("Invalid input enter either 1=yes, 2=no.");
+                    }
+            }
+*/
         }while(!validEntry);
+        System.out.println("Login Successful!");
         return travelerAccount;
     }
 
@@ -280,7 +326,7 @@ public class TravelProgram{
             System.out.println("*********************");
             System.out.println("Create Account");
             System.out.println("*********************");
-
+            System.out.println("Password must be more than 8 characters and contain at least 1 Uppercase, 1 Lowercase, 1 number, and 1 symbol [@#$%^&+=]");
             System.out.println("\tEnter email and password below");
             System.out.print("\tEmail: ");
             email = scanner.nextLine();
